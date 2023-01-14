c7043f3448a9b0a8b47686ffbf14db67ba6ea353
jacob-d242 <jacobmoracha@gmail.com>
2023-01-15 00:38:44 +0300

    updating app to add state and login flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
-import React from 'react'
 import './index.css'
-import Login from './components/Login'
-function App() {
+import { useState, useEffect } from 'react'
+import { supabase } from './supabaseClient'
+import Auth from './Auth'
+import Account from './Account'
+
+export default function App() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
   return (
-    <div>
-     <Login/>
+    <div className="container" style={{ padding: '50px 0 100px 0' }}>
+      {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
     </div>
   )
 }
-
-export default App
