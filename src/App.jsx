20fb8ab6313c0b2cd238ed4361570a8462103548
jacob-d242 <jacobmoracha@gmail.com>
2023-01-15 00:35:05 +0300

    Updates login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import React from 'react'
-
+import './index.css'
+import Login from './components/Login'
 function App() {
   return (
     <div>
-      <h1>asdfg</h1>
+     <Login/>
     </div>
   )
 }
