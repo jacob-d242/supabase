70034698c0a40b7fa24671ada5e56972292f5594
jacob-d242 <jacobmoracha@gmail.com>
2023-01-17 10:48:10 +0300

    Craating app with CRA

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,25 @@
 import React,{ useState, useEffect } from 'react'
 import SignIn from './components/SignIn'
 //import { supabase } from './constants/supaBaseClient'
-//import Login from './components/Login'
 
 export default function App() {
-  //const [session, setSession] = useState(null)
+  const [session, setSession] = useState(null)
 
-  //useEffect(() => {
-  //  supabase.auth.getSession().then(({ data: { session } }) => {
-  //    setSession(session)
-  //  })
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
 
-  //  supabase.auth.onAuthStateChange((_event, session) => {
-  //    setSession(session)
-  //  })
-  //}, [])
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
 
   return (
     <div className="container" style={{ padding: '50px 0 100px 0' }}>
       <h1>moracha</h1>
-      <SignIn/>
-      {/*<Login/>*/}
+      {/*<SignIn />*/}
+      <h1>{process.env.REACT_APP_SUPABASE_ANON_KEY }</h1>
       {/*{!session ? <Login /> : <Account key={session.user.id} session={session} />}*/}
     </div>
   )
--- src/constants/supaBaseClient.jsx
+++ src/constants/supaBaseClient.jsx
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
-
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 
-export const supabase = createClient (supabaseAnonKey,supabaseUrl)
+
+export const supabase = createClient (supabaseUrl,supabaseAnonKey)
